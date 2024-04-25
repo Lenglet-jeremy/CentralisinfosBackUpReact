257a545054eaee06d9a6af0440324ee754c84766
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-04-25 20:05:21 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import NavBar from './components/NavBar'
+import Main from './components/Main'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <NavBar />
+    <Main />
+    <Footer />
     </>
   )
 }
