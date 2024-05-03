0794ad9db2a2e3a832dd5a3ea3614c61ca1c8f0e
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-05-03 16:10:17 +0200

    Mise à jour NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <>
-    <NavBar />
-    </>
+    <div className={`AppDark`}>
+      <NavBar />
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,51 @@
 import "./NavBar.css"
-import LOGO from "../../Img/LogoCentralisinfos/LOGO.svg"
 
-export default function NavBar(){
+
+export default function NavBar({handleTheme}){
+
+
     return(
-        <nav class="NavBarArea">
-            <div class="LeftSide">
-                <img src={LOGO} alt="LOGO" />
-                <h2>Centralisinfos</h2>
-                <ul>
-                    <li><a href="#">Enseignements par Discipline</a></li>
-                    <li><a href="#">Enseignements par Niveaux</a></li>
-                    <li><a href="#">Cursus Universitaires</a></li>
-                </ul>
-            </div>
-            <div class="RightSide">
-                <ul>
-                    <p>theme toggle</p>
-                    <li><a href="#">Tutos</a></li>
-                    <li><a href="#">Forum</a></li>
-                    <li><a href="#">Compte</a></li>
-                </ul>
-            </div>
-        </nav>
+        <nav class="NavBar">
+          <ul class="LeftSide">
+              <a href="#" className={`logo`}/>
+                <li class="Skills"><a href="#">Enseignements par Niveaux</a>
+                    <ul class="SkillsDropDown">
+                        <li><a href="#">Sixieme</a></li>
+                        <li><a href="#">Cinquiete</a></li>
+                        <li><a href="#">Quatrieme</a></li>
+                        <li><a href="#">Troisieme</a></li>
+                        <li><a href="#">Seconde</a></li>
+                        <li><a href="#">Premiere</a></li>
+                        <li><a href="#">Terminale</a></li>
+                    </ul>
+                </li>
+                <li class="Subjects"><a href="#">Enseignements par Disciplines</a>
+                    <ul class="SubjectsDropDown">
+                        <li><a href="#">Français</a></li>
+                        <li><a href="#">Anglais</a></li>
+                        <li><a href="#">Maths</a></li>
+                    </ul>
+                </li>
+                <li class="UniversityCourses"><a href="#">Cursus Universitaires</a>
+                    <ul class="UniversityCoursesDropDown">
+                        <li><a href="#">Developpement Web</a></li>
+                        <li><a href="#">Comptabilité et Gestion</a></li>
+                    </ul>
+                </li>
+          </ul>
+
+          <ul class="RightSide">
+            {/* ========================================================= */}
+            <label class="DarkLightThemeToggle">
+                <input type="checkbox"/>
+                <span class="DarkLightThemeSlider"></span>
+            </label>
+            {/* ========================================================= */}
+            <li><a href="#">Tutos</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">Compte</a></li>
+          </ul>
+      </nav>
+      
     )
 }
