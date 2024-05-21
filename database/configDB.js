b3454bf754401d6c7da60974b4c20cd0bc641cc6
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-05-21 09:31:00 +0200

    Delete database directory

--- database/configDB.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoDB: {
-        uri: "mongodb+srv://lengletjeremyperso:<password>@clustertest.kgw7zge.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTest",
-    },
-};
