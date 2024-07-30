18c929fb878ad192d343ce5c58268775ce153781
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-07-30 13:18:56 +0200

    Réinitialisation

--- Tests/useIncrement.js
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from "react";
-
-export function useIncrement({base = 0, max = Infinity, min = -Infinity} = {}) {
-    const [state, setState] = useState(base);
-    const increment = useCallback(() => setState((v) => Math.min(v + 1, max)), [max]);
-    const decrement = useCallback(() => setState((v) => Math.max(v - 1, min)), [min]);
-    return {state, increment, decrement};
-}
