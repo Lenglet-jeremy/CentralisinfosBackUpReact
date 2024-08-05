4bfa974330f3d3d9a5042c84c4fdef7ef8ea7c47
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-08-05 16:59:21 +0200

    Mise à jour

--- src/pages/3.Tutos/2.OpenPyXl/3.0.ExcelCalendInit.jsx
+++ src/pages/3.Tutos/2.OpenPyXl/3.0.ExcelCalendInit.jsx
@@ -1,14 +1,17 @@
-import "./3.0.ExcelCalendInit.css"
-import { useContext } from "react";
+import "./3.0.ExcelCalendInit.css";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../../ThemeContext";
 import NavBarPrimary from "../../../components/1.NavBars/1.NavBarPrimary/NavBarPrimary";
 import NavBarSecondary from "../../../components/1.NavBars/2.NavBarSecondary/NavBarSecondary";
 
-export default function ExcelCalendInit(){
+import ExcelInitialisation from "./img/initialisation-Excel.webp";
+
+export default function ExcelCalendInit() {
     const { theme, toggleTheme } = useContext(ThemeContext);
+    const [detailsLevel, setDetailsLevel] = useState('élevé'); // état pour gérer le niveau de détails
 
     const bodyTheme = theme === "DarkTheme" ? "DarkBody" : "LightBody";
-    return(
+    return (
         <div>
             <NavBarPrimary handleTheme={toggleTheme} />
             <NavBarSecondary />
@@ -18,11 +21,55 @@ export default function ExcelCalendInit(){
                     <div className="Sommary">
                         <p>Sommary</p>
                     </div>
-
+                    
                     <h2>Initialisation du projet</h2>
-                    <p className="DetailsEleve">Commencez par ouvrir un fichier Excel</p>
+                    
+                    {/* Boutons de sélection du niveau de détails */}
+                    <div className="details-buttons">
+                        <button onClick={() => setDetailsLevel('faible')}>détails : faible</button>
+                        <button onClick={() => setDetailsLevel('moyen')}>détails : moyen</button>
+                        <button onClick={() => setDetailsLevel('élevé')}>détails : élevé</button>
+                    </div>
+                    
+                    <div className="CourseBody">
+                        <p>Commencez par ouvrir un fichier Excel</p>
+                        <p>Nous allons commencer par formater la largeur des colonnes B à AK (B:AK)</p>
+                        
+                        <p>La colonne A nous servira plus tard comme panneau de controle</p>
+
+                        {detailsLevel === 'élevé' && (
+                            <p>Pour ajuster la largeur d'un colonne 
+                            vous devez rester cliquer sur la barre latéral de droite de la colonne 
+                            que vous souhaitez redimmentionner et glisser votre souris de droite à gauche
+                            pour augmenter/reduire la taille de la colonne</p>
+                        )}
+                        
+                        {   detailsLevel === 'élevé' && (
+                        <img src={ExcelInitialisation} alt="Initialisation Excel" />
+                        )}
+                        
+                        
+                        {detailsLevel !== 'faible' && (
+                            <div>
+                                <p> Si vous souhaitez redimmentionner plusieurs colonnes vous avez deux possibilités : </p>
+                                <pre>    Selectionner la colonne B, rester appuyé sur shift, la touche juste au dessus de votre touche Ctrl (tout en bas à gauche de votre clavier), <br />
+                                celà aura pour effet de selectionner toutes les colonnes entre la colonne B et la dernière sur laquelle vous aurez cliqué</pre>
+                                <pre>    Rester appuyé sur Ctrl (tout en bas à gauche de votre clavier), celà aura pour effet de selectionner les colonnes sur lesquelles vous aurez cliqué</pre>
+                            </div>
+                        )}
+
+                        {detailsLevel !== 'faible' && (
+                            <div>
+                                <p>Commencer par paramétrer la colonne B d'une largeur de 2 unités (19 pixels),la colonne C d'une largeur de 4,14 unités (34 pixels) et la colonne D à 12,57 unités (93 pixels)</p>
+                                <p>Ensuite, cliquer sur la colonne B et tout en restant appuyé allez jusqu'a la colonne D, cliquez sur "Reproduire la mise en forme" puis selectionné la plage de colonne E:AK</p>
+                                <iframe src="https://onedrive.live.com/embed?resid=F7CF7201558D3C58%211184&authkey=!AOgbGlgeeAqeYCM" width="1080" height="320" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                            </div>
+                        )}
+
+
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
